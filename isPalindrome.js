23750fcf7df78c57767e1c34d592366436d3c882
DanielleSmith0 <daniellesmith@Danielles-MBP.localdomain>
2023-02-23 10:32:33 -0700

    walkthrough added

--- isPalindrome.js
+++ isPalindrome.js
@@ -13,4 +13,41 @@ const palindrome = str => {
     }
 }
 
-console.log(palindrome("mom"));
+console.log(palindrome("Mob"));
+
+//Answer walkthrough
+//1. turn string into array, reverse, and compare.
+
+const isPalindrome = (str) => {
+    return strRev = str.split('').reverse().join('') === str
+
+}
+
+console.log(isPalindrome("asdf"))
+
+//2. Make a fxn that takes a str
+//Take str and turn into an array called "word"
+//take 2 empty arrays, named forward and backwards
+//Make a loop: loops through the "word" array
+    //push the current index into backwards
+    //unshift the current index into forwards
+//turn forwards and backwards into strings
+//if forward backwards are equal, return true, else return false
+
+function isPalindrome2(str) {
+    let word = str.split('')
+    let forwards = [];
+    let backwards = [];
+    for(let i = 0; i < word.length; i++) {
+        backwards.push(word[i]);
+        forwards.unshift(word[i]);
+    }
+    fString = forwards.join('');
+    bString = backwards.toLocaleString('');
+    if (fString === bString) {
+        return true;
+    //you don't need to else return false, because after a return, the fxn exits and false will become automatic, if outside fxn.
+    }
+    
+    return false
+}
